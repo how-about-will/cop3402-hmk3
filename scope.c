43a0803ff559f9a23c06de2d8996fef6ee9747f4
tojakar <167592470+tojakar@users.noreply.github.com>
2024-11-10 20:32:37 -0500

    added the scope.c functions

--- scope.c
+++ scope.c
@@ -17,3 +17,51 @@ scope_t* scope_create(){
         new_s->entries[i] = NULL;
     }
 }
+
+//returns number of variable and constant declarations that are in this scope
+extern unsigned int scope_loc_count(scope_t* s){
+    return s->loc_count;
+}
+
+// Return the current scope's size (number of declared ids).
+unsigned int scope_size(scope_t *s){
+    return s->size;
+}
+
+// Is the current scope full?
+bool scope_full(scope_t *s){
+    return scope_size(s) >= MAX_SCOPE_SIZE;
+}
+
+//adds a association to the scope
+static void scope_add(scope_t *s, scope_assoc_t *assoc){
+    assoc->attrs->offset_count = (s->loc_count)++;
+    s->entries[(s->size)++] = assoc;
+}
+
+//crates a new association and adds it the scope
+void scope_insert(scope_t* s, const char* name, id_attrs* attrs) {
+    scope_assoc_t *new_assoc = malloc(sizeof(scope_assoc_t));
+    if (new_assoc == NULL) {
+	bail_with_error("No space for association!");
+    }
+    new_assoc->id = name;
+    new_assoc->attrs = attrs;
+    scope_add(s, new_assoc);
+}
+
+//checks to see if the name has already been declared in the current scope
+bool scope_declared(scope_t *s, const char *name)
+{
+    return scope_lookup(s, name) != NULL;
+}
+
+//returns a pointer to the attributes if the name exists in the scope otherwise returns null
+id_attrs *scope_lookup(scope_t *s, const char *name){
+    for(int i = 0; i < s->size){
+        if(strcmp(name,s->entries[i]->id)){
+            return s->entries[i]->attrs;
+        }
+    }
+    return NULL;
+}
