bb94758a2bc030905d45d450eee2f2293bc86914
Kietvu2279 <vutuankiet279@gmail.com>
2024-11-11 23:01:03 -0500

    Update scope_check.c - finish scope_check_const_decls

--- scope_check.c
+++ scope_check.c
@@ -25,8 +25,27 @@ block_t scope_check_program(block_t prog){
 //scope checks const_decls
 void scope_check_const_decls(const_decls_t decls){
     const_decl_t* constDecl = decls.start;
+    unsigned int offset_count = 0;
     while(constDecl != NULL){
-        scope_check_const_delc(*constDecl->next);
+        //chech each def in def_list
+        const_def_t* constDef = constDecl->const_def_list.start;
+
+        while(constDef != NULL){
+            const char* name = constDef->ident.name;
+
+            id_attrs *attrs = create_id_attrs(*constDef->ident.file_loc, constant_idk, offset_count);
+
+            //check for already declared error
+            if(!symtab_declared_in_current_scope(name) && attrs != NULL){
+                symtab_insert(name, attrs);
+            }
+            else{
+                bail_with_error("Constant '%s' is already declared in this scope", name);
+            }
+            //scope_check_const_delc(*constDecl->next); I don't think we need this
+            constDef = constDef->next;
+            offset_count++;
+        }
         constDecl = constDecl->next;
     }
 }
